b95faf892ebf8f5fd6a5f9adfa7300dc4dfaabef
Feyto <feyto81@gmail.com>
2021-04-08 04:32:50 +0700

    adding api Food:

--- app/Http/Controllers/API/FoodController.php
+++ app/Http/Controllers/API/FoodController.php
@@ -2,10 +2,57 @@
 
 namespace App\Http\Controllers\API;
 
-use App\Http\Controllers\Controller;
+use App\Models\Food;
 use Illuminate\Http\Request;
+use App\Helpers\ResponseFormatter;
+use App\Http\Controllers\Controller;
 
 class FoodController extends Controller
 {
-    //
+    public function all(Request $request)
+    {
+        $id = $request->input('id');
+        $limit = $request->input('limit', 6);
+        $name = $request->input('name');
+        $types = $request->input('types');
+        $price_from = $request->input('price_form');
+        $price_to = $request->input('price_to');
+        $rate_from = $request->input('rate_form');
+        $rate_to = $request->input('rate_to');
+
+        if ($id) {
+            $food = Food::find($id);
+
+            if ($food) {
+                return ResponseFormatter::success(
+                    $food,
+                    'Data produk berhasil diambil'
+                );
+            } else {
+                return ResponseFormatter::error(
+                    null,
+                    'Data produk tidak ada',
+                    404
+                );
+            }
+        }
+
+        $food = Food::query();
+
+        if ($name) {
+            $food->where('name', 'like', '%' . $name, '%');
+        }
+
+        if ($types) {
+            $food->where('types', 'like', '%' . $types, '%');
+        }
+
+        if ($price_from) {
+            $food->where('price', '>=', $price_from);
+        }
+
+        if ($price_to) {
+            $food->where('price', '<=', $price_to);
+        }
+    }
 }
