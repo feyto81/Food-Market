3a8f7683d5f219d430473d08e93fbe4371b301d2
Feyto <feyto81@gmail.com>
2021-04-07 20:34:18 +0700

    change food model attribute

--- app/Models/Food.php
+++ app/Models/Food.php
@@ -2,10 +2,26 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Carbon\Carbon;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class Food extends Model
 {
-    use HasFactory;
+    use HasFactory, SoftDeletes;
+
+    protected $fillable = [
+        'name', 'description', 'ingredients', 'price', 'rate', 'types'
+    ];
+
+    public function getCreatedAttribute($value)
+    {
+        return Carbon::parse($value)->timestamp;
+    }
+
+    public function getUpdatedAttribute($value)
+    {
+        return Carbon::parse($value)->timestamp;
+    }
 }
