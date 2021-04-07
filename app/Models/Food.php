3c26af5db51524461e273dad89e12cffc9380791
Feyto <feyto81@gmail.com>
2021-04-07 20:42:38 +0700

    adding picturepath Attributes

--- app/Models/Food.php
+++ app/Models/Food.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Carbon\Carbon;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\Storage;
 use Illuminate\Database\Eloquent\SoftDeletes;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 
@@ -12,7 +13,8 @@ class Food extends Model
     use HasFactory, SoftDeletes;
 
     protected $fillable = [
-        'name', 'description', 'ingredients', 'price', 'rate', 'types'
+        'name', 'description', 'ingredients', 'price', 'rate', 'types',
+        'picturePath'
     ];
 
     public function getCreatedAttribute($value)
@@ -23,5 +25,17 @@ class Food extends Model
     public function getUpdatedAttribute($value)
     {
         return Carbon::parse($value)->timestamp;
+    }
+
+    public function toArray()
+    {
+        $toArray = parent::toArray();
+        $toArray['picturePath'] = $this->picturePath;
+        return  $toArray;
+    }
+
+    public function getPicturePathAttribute()
+    {
+        return url('') . Storage::url($this->attributes['picturePath']);
     }
 }
