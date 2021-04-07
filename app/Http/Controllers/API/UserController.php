686e1d00fba9b49d11f49e10db4c92108d9d3a39
Feyto <feyto81@gmail.com>
2021-04-08 03:22:33 +0700

    add api uploadPhoto

--- app/Http/Controllers/API/UserController.php
+++ app/Http/Controllers/API/UserController.php
@@ -10,6 +10,7 @@ use App\Helpers\ResponseFormatter;
 use App\Http\Controllers\Controller;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Hash;
+use Illuminate\Validation\Validator;
 
 class UserController extends Controller
 {
@@ -100,5 +101,26 @@ class UserController extends Controller
         $user = Auth::user();
         $user->update($data);
         return ResponseFormatter::success($user, 'Profile Updated');
+    }
+
+    public function updatePhoto(Request $request)
+    {
+        $validator = Validator::make($request->all(), [
+            'file' => 'required|image|max:2048'
+        ]);
+        if ($validator->fails()) {
+            return ResponseFormatter::error(
+                ['error' => $validator->errors()],
+                'Update photo fails',
+                401
+            );
+        }
+        if ($request->file('file')) {
+            $file = $request->file->store('assets/user', 'public');
+            $user = Auth::user();
+            $user->profile_photo_path = $file;
+            $user->update();
+            return ResponseFormatter::success([$file], 'File successfully uploaded');
+        }
     }
 }
