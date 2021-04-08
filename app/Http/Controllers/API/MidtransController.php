7088557a1a9591240011538d6db47ed9c8ebe06b
Feyto <feyto81@gmail.com>
2021-04-09 00:05:10 +0700

    add

--- app/Http/Controllers/API/MidtransController.php
+++ app/Http/Controllers/API/MidtransController.php
@@ -2,10 +2,20 @@
 
 namespace App\Http\Controllers\API;
 
-use App\Http\Controllers\Controller;
+use Midtrans\Config;
 use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
 
 class MidtransController extends Controller
 {
-    //
+    public function callback(Request $request)
+    {
+        //set konfigurasi midtrans
+        Config::$serverKey = config('services.midtrans.serverKey');
+        Config::$isProduction = config('services.midtrans.isProduction');
+        Config::$isSanitized = config('services.midtrans.isSanitized');
+        Config::$is3ds = config('services.midtrans.is3ds');
+
+        // Buat instanse midtrans notification
+    }
 }
