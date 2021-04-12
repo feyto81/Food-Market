19352b493e7963d3583c61d19acd13402bf8b74a
Feyto <feyto81@gmail.com>
2021-04-12 12:12:01 +0700

    add

--- app/Http/Controllers/API/UserController.php
+++ app/Http/Controllers/API/UserController.php
@@ -87,12 +87,22 @@ class UserController extends Controller
     public function register(Request $request)
     {
         try {
-            $request->validate([
+            // yang asli
+            // $request->validate([
+            //     'name' => ['required', 'string', 'max:255'],
+            //     'email' => ['required', 'string', 'email', 'max:255', 'unique:users'],
+            //     'password' => $this->passwordRules()
+            // ]);
+            //yang baru
+            $validator = Validator::make($request->all(), [
                 'name' => ['required', 'string', 'max:255'],
                 'email' => ['required', 'string', 'email', 'max:255', 'unique:users'],
                 'password' => $this->passwordRules()
             ]);
 
+            if ($validator->fails()) {
+                return response()->json($validator->errors(), 400);
+            }
             User::create([
                 'name' => $request->name,
                 'email' => $request->email,
